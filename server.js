4b812218364576f4679f0dc5a7fa11a72d9c9865
JJ <jh_86@hotmail.co.uk>
2021-05-05 07:41:58 +0100

    coded out port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose= require('mongoose'); //helps use connect to mongoose database
 require("dotenv").config(); 
 
 const app = express();
-const port=  5000; //process.env.PORT ||
+const port=   process.env.PORT ||5000;
 
 app.use(cors()); //middlewear
 app.use(express.json());
